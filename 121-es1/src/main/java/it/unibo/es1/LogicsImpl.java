f85a88dc6eef82f3b4be93e34cd1aefc8deb891f
Nicolas Tazzieri - nicolas.tazzieri@studio.unibo.it <nicolas.tazzieri@studio.unibo.it>
2024-12-10 16:33:55 +0100

    Finished ex.1

--- 121-es1/src/main/java/it/unibo/es1/LogicsImpl.java
+++ 121-es1/src/main/java/it/unibo/es1/LogicsImpl.java
@@ -1,46 +1,56 @@
 package it.unibo.es1;
 
+import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class LogicsImpl implements Logics {
 
-	public LogicsImpl(int size) {
-		//TODO Auto-generated constructor stub
+	private final List<Integer> values;
+
+	public LogicsImpl(final int size) {
+		if (size < 0) {
+			throw new IllegalArgumentException("The size is lower than 0");
+		}
+		values = new ArrayList<>(size);
+		for (int i = 0; i < size; i++) {
+			values.add(0);
+		}
 	}
 
 	@Override
 	public int size() {
-		// TODO Auto-generated method stub
-		throw new UnsupportedOperationException("Unimplemented method 'size'");
+		return values.size();
 	}
 
 	@Override
 	public List<Integer> values() {
-		// TODO Auto-generated method stub
-		throw new UnsupportedOperationException("Unimplemented method 'values'");
+		return List.copyOf(values);
 	}
 
 	@Override
 	public List<Boolean> enablings() {
-		// TODO Auto-generated method stub
-		throw new UnsupportedOperationException("Unimplemented method 'enablings'");
+		return values.stream()
+			.map(v -> v < size())
+			.toList();
 	}
 
 	@Override
-	public int hit(int elem) {
-		// TODO Auto-generated method stub
-		throw new UnsupportedOperationException("Unimplemented method 'hit'");
+	public int hit(final int elem) {
+		final Integer oldValue = values.remove(elem);
+		values.add(elem, oldValue + 1);
+		return values.get(elem);
 	}
 
 	@Override
 	public String result() {
-		// TODO Auto-generated method stub
-		throw new UnsupportedOperationException("Unimplemented method 'result'");
+		return values.stream()
+			.map(v -> Integer.toString(v))
+			.collect(Collectors.joining("|", "<<", ">>"));
 	}
 
 	@Override
 	public boolean toQuit() {
-		// TODO Auto-generated method stub
-		throw new UnsupportedOperationException("Unimplemented method 'toQuit'");
+		return values.stream().allMatch(v -> v == values.getFirst() && v != 0);
 	}
 }
